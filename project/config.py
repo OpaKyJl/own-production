40b5c44b8098fa3aebb9ac7a6daa7695fbf5669f
OpaKyJl <danila.pavlovv@mail.ru>
2023-11-16 18:58:50 +0800

    начало работы с БД

--- project/config.py
+++ project/config.py
@@ -3,4 +3,3 @@ user = "postgres"
 password = "1234"
 db_name = "own_products_db"
 port = 5432
-schemas = "own_products_schema"
--- project/server.py
+++ project/server.py
@@ -1,5 +1,5 @@
 import psycopg2
-from config import host, user, password, db_name, schemas
+from config import host, user, password, db_name
 
 connection = None
 cursor = None
